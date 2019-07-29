f14e49c86a86bb428fea510c93e887532d9a5064
juzhiyuan <jjzhiyuan@gmail.com>
2019-07-29 08:14:51 +0000

    866617fd: Fiix ux (#8)
    * naviback to list page
    
    * add action
    
    * add i18n
    
    * add validator

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.483da931a1d4748b66ef971d2d3fbc59.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.b960672a657edfc96fa1aae15d2fd04b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
