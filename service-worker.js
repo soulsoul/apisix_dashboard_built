6fe5675af8a7b9c98e6f42f37e869ab2bcabc3ce
juzhiyuan <jjzhiyuan@gmail.com>
2019-08-05 10:04:22 +0000

    6657673b: fix show upstream desc

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/apisix/dashboard/precache-manifest.879039edeb9d84fd0add15eecca283c5.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/apisix/dashboard/precache-manifest.0284de69006f3cd5f0cc280e04731e57.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
