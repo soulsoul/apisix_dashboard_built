3e56914af36b4f80ffcf8a908a50be75d8b27b53
juzhiyuan <jjzhiyuan@gmail.com>
2019-08-11 12:10:14 +0000

    ec4e39fc: fix #24/#25

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/apisix/dashboard/precache-manifest.a5643adb231bcd44760c8cfded4d8d11.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/apisix/dashboard/precache-manifest.404501ac89f4f5ce92bb438f1fc5d818.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
